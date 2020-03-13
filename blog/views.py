ab27954a6e339bcd79650e443daa982640e94db5
iman-sedgh <iman.sedgh@yahoo.com>
2020-03-14 01:26:18 +0330

    Create, Update, and Delete Posts Works Fine At the end of 10th video

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.views.generic import ListView, DetailView,CreateView,UpdateView,DeleteView
 from .models import Post
 
 # Create your views here.
@@ -9,31 +10,49 @@ def home(request):
   }
   return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+  model = Post
+  template_name = 'blog/home.html'
+  context_object_name = 'posts'
+  ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+  model = Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+  model = Post
+  fields = ['title','content']
+
+  def form_valid(self,form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+  model = Post
+  fields = ['title','content']
+
+  def form_valid(self,form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+
+  def test_func(self):
+    post = self.get_object()
+    if self.request.user == post.author:
+      return True
+    return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+  model = Post
+  success_url = '/'
+  def test_func(self):
+    post = self.get_object()
+    if self.request.user == post.author:
+      return True
+    return False
+
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
 
-    
-def test(request):
-   return HttpResponse("""<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
-</html>""")
